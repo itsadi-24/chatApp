a25aa2ad3272cbb3ce3b7eb8512f541fbc08d579
itsadi-24 <158060025+itsadi-24@users.noreply.github.com>
2024-05-31 19:52:02 +0530

    AuthContext setup done

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        // the target is gonna be prefixed when we hit api
+        target: 'http://localhost:3000',
+      },
+    },
+  },
+});
